fac62beb59e5c7cfcfe0b22396e8de923bc0c19e
19rafsan97 <rafsanrahman35549@gmail.com>
2025-08-17 23:07:52 +0930

    final pipeline updated

---
+++ main.py
@@ -0,0 +1,9 @@
+from gui import launch_gui
+# from notifier import start_notifier   # Uncomment if you want background notifier
+
+if __name__ == "__main__":
+    # Run Tkinter App
+    launch_gui()
+
+    # Optional: Start notifier in background
+    # start_notifier()
